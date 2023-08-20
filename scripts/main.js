005cd047785e4b11247bfdf6b3ec567b482ba62d
Tergrad <142720985+Tergrad@users.noreply.github.com>
2023-08-20 14:35:33 +0200

    Es funktioniert!

--- scripts/main.js
+++ scripts/main.js
@@ -1,77 +1,48 @@
-//  Sidebar: Listen for click on toggle Sidebar button; Check if 'quickGencontent' div is displayed; If hidden or net set
-//           it will show, if it's shown, it will hide it.
-document.getElementById('toggleHandle').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar.classList.contains('expanded')) {
-        sidebar.classList.remove('expanded');
-    } else {
-        sidebar.classList.add('expanded');
-    }
-});
-
-
-// Handle button click of Quick random generation Button
-document.getElementById('generateRandom').addEventListener('click', function() {
-    const randomNumber = Math.floor(Math.random() * 100); // Generates a random number between 0-99
-    document.getElementById('randomResult').innerText = randomNumber;
-});
-
-
-
-
-//  HTML to Function Link
-
-document.getElementById('booksCard').addEventListener('click', function() {
-    const cardContent = this.querySelector('.cardContent');
-    if (cardContent.style.display === "none" || cardContent.style.display === "") {
-        cardContent.style.display = "block";
-
-        // Populate content (This is a simple example, you can enhance this)
-        let contentHtml = "<strong>Types:</strong><br>";
-        booksData.types.forEach(type => {
-            contentHtml += `<span>${type}</span><br>`;
-        });
-        contentHtml += "<strong>Eras:</strong><br>";
-        booksData.eras.forEach(era => {
-            contentHtml += `<span>${era}</span><br>`;
-        });
-        cardContent.innerHTML = contentHtml;
-
-    } else {
-        cardContent.style.display = "none";
-    }
-});
-
-
-
-
-// Fetch the book data from the JSON file
-
-let bookData;
-
-fetch('Data/books.json')
-    .then(response => response.json())
-    .then(data => {
-        bookData = data;
-    })
-    .catch(error => console.error('Error fetching the JSON data:', error));
-
-    function populateGenresDropdown() {
-        const genreSelector = document.getElementById('genreSelector');
-        
-        for (const genre in bookData) {
-            const option = document.createElement('option');
-            option.value = genre;
-            option.textContent = genre;
-            genreSelector.appendChild(option);
-        }
-    }
-
-
+// Helper function to get a random item from an array
 function getRandomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
+
+
+let bookData = {};
+
+// Fetch JSON data
+fetch('Data/books.json')
+    .then(response => response.json())
+    .then(data => {
+        bookData = data;
+        initialize();
+    })
+    .catch(error => {
+        console.error("Error fetching book data:", error);
+    });
+
+// This function initializes your application after the data has been loaded
+function initialize() {
+    // Populate genre dropdown
+    const genreSelector = document.getElementById('genreSelector');
+    for (const genre in bookData) {
+        const option = document.createElement('option');
+        option.value = genre;
+        option.textContent = genre;
+        genreSelector.appendChild(option);
+    }
+
+    // Handle book name generation
+    const generateBtn = document.getElementById('generateBtn');
+    generateBtn.addEventListener('click', function() {
+        const genre = genreSelector.value;
+        const name = generateBookName(genre);
+        document.getElementById('generatedName').innerText = name;
+    });
+}
+
+// ... (rest of your functions like getRandomItem and generateBookName)
+
+
+
+// Main function to generate book name
 function generateBookName(genre) {
     if (!bookData || !bookData[genre]) {
         console.error("Book data not loaded yet or Invalid Genre");
@@ -86,10 +57,24 @@ function generateBookName(genre) {
         while (title.includes(`[${placeholder}]`)) {
             let replacement;
 
-            if (genre === "WissenschaftlicheBucher" && (placeholder === "Substantive" || placeholder === "Adjektive")) {
+            // Special probability logic for Präfixe and Suffixe
+            if (placeholder === "Präfix" || placeholder === "Suffix") {
+                if (Math.random() > 0.7) {
+                    replacement = getRandomItem(genreData.placeholders[placeholder]);
+                } else {
+                    // Handle potential surrounding hyphens when removing the placeholder
+                    const regex = new RegExp(`-?\\[${placeholder}\\]-?`, 'g');
+                    title = title.replace(regex, "");
+                    continue;
+                }
+            } 
+            // Handle Substantiv and Adjektiv for WissenschaftlicheBucher genre
+            else if (genre === "WissenschaftlicheBucher" && (placeholder === "Substantiv" || placeholder === "Adjektiv")) {
                 const discipline = getRandomItem(genreData.placeholders["Disziplin"]);
-                replacement = getRandomItem(genreData.placeholders[placeholder][discipline] || genreData.placeholders[placeholder].default);
-            } else {
+                replacement = getRandomItem(genreData.placeholders[placeholder][discipline]);
+            } 
+            // Default case
+            else {
                 replacement = getRandomItem(genreData.placeholders[placeholder]);
             }
 
@@ -99,13 +84,26 @@ function generateBookName(genre) {
     return title;
 }
 
-<button onclick="showGeneratedBookName()" id="generateBtn">Generate Book Name</button>
 
 
 
 
-function showGeneratedBookName() {
-    const genre = document.getElementById('genreSelector').value;
-    const name = generateBookName(genre);
-    document.getElementById('generatedName').innerText = name;
-}
+// Event listeners and other setup logic
+document.addEventListener("DOMContentLoaded", function() {
+    // Populate genre dropdown
+    const genreSelector = document.getElementById('genreSelector');
+    for (const genre in bookData) {
+        const option = document.createElement('option');
+        option.value = genre;
+        option.textContent = genre;
+        genreSelector.appendChild(option);
+    }
+
+    // Handle book name generation
+    const generateBtn = document.getElementById('generateBtn');
+    generateBtn.addEventListener('click', function() {
+        const genre = genreSelector.value;
+        const name = generateBookName(genre);
+        document.getElementById('generatedName').innerText = name;
+    });
+});
